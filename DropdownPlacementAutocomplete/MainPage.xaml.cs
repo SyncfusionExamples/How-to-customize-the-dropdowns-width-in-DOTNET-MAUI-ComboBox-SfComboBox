651185a11fe6ba1d9686f6d0d0d8ee16676cf8ba
KishoreJey <156897485+KishoreJey@users.noreply.github.com>
2024-06-21 16:41:48 +0530

    Updated Autocomplete sample.

---
+++ DropdownPlacementAutocomplete/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace DropdownwidthMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    Widthslider.Value = e.NewValue;
+        //    Combobox.DropdownWidth = Widthslider.Value;
+        //    Combobox1.DropdownWidth = Widthslider.Value;
+        //    WidthLabel.Text = $"DropdownWidth : {(int)Widthslider.Value}";
+        //}
+    }
+
+}
